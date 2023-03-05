87ac1ec1d6e46ae0740255309ab2c222d5511d39
Igor Md <beerk0ff@yandex.ru>
2023-03-05 18:25:19 +0500

    2I

--- Лекция 2. «Линейный поиск»/I.py
+++ Лекция 2. «Линейный поиск»/I.py
@@ -1,32 +1,28 @@
 def input_read():
     n, m, k = [int(x) for x in input().split()]
-    mines = [[int(x) - 1 for x in input().split()] for _ in range(k)]
+    mines = [[int(x) for x in input().split()] for _ in range(k)]
     return n, m, k, mines
 
 
 def render_field(n, m, k, mines):
-    field = [[0] * m for _ in range(n)]
+    field = [[0] * (m + 2) for _ in range(n + 2)]
     neibs_x = [0, -1, -1, -1, 0, 1, 1, 1]
     neibs_y = [-1, -1, 0, 1, 1, 1, 0, -1]
     for x, y in mines:
-        field[x][y] = '*'
         for k in range(8):
             neib_x = x + neibs_x[k]
             neib_y = y + neibs_y[k]
-            if neib_x < 0 or neib_x >= n:
-                continue
-            if neib_y < 0 or neib_y >= m:
-                continue
-            if field[neib_x][neib_y] == '*':
-                continue
             field[neib_x][neib_y] += 1
+    for x, y in mines:
+        field[x][y] = '*'
+
     return field
 
 
 def main():
     field = render_field(*input_read())
-    for line in field:
-        print(*line)
+    for line in field[1:-1]:
+        print(*line[1:-1])
 
 
 if __name__ == '__main__':
